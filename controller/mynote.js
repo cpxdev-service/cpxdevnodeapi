67207b7b9ef51ac479a9c6895482c2cc8cee4eca
Chinnathorn P <chinnathorn.p@gmail.com>
2024-05-17 22:27:43 +0700

    Update library and api

--- controller/mynote.js
+++ controller/mynote.js
@@ -1,34 +1,34 @@
 const { Router } = require('express'); 
 const app = Router(); 
-const mysql = require('mysql');
-const connection = mysql.createConnection({
-    host     : 'cpxdevdb-cpxdevservice.aivencloud.com',
-    user     : 'avnadmin',
-    password : 'AVNS_ysCQDNAWaVQYdvKTwma',
-    database : 'nodejsdemo',
-    port: 21023,
-    ssl : true
-  });
-
+const { createPool} = require('mysql2');
+const connection = createPool('mysql://avnadmin:AVNS_ysCQDNAWaVQYdvKTwma@cpxdevdb-cpxdevservice.aivencloud.com:21013/nodejsdemo');
 const basepath = '/mynote'
 
 app.get(basepath + '/', (req, res) => {
-    connection.connect();
-    connection.query('SELECT * from addNote', function (error, results, fields) {
-        if (error) {
-            connection.end();
-            res.json({
-                status: false,
-                response: error
-            })
-        };
-        connection.end();
-        res.json({
-            status: true,
-            response: results
-        })
-    });
-})
+  connection.query('SELECT * from addNote', function (error, results, fields) {
+    if (error) {
+      res.status(502).json({
+        status: false,
+        response: error
+      });
+      return;
+    }
+
+    if (results == undefined) {
+    res.json({
+        status: true,
+        response: []
+      });
+      return;
+    }
+
+    res.json({
+        status: true,
+        response: results
+      });
+
+  });
+});
 app.get(basepath + '/:noteId', (req, res) => {
     res.json({
         status: true

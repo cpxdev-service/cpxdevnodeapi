e58998550ef867df617482b69d9a0769cd2ed6c0
chinnathorn.pro <chinnathorn.pro@cubeofnine.asia>
2024-05-17 17:04:22 +0700

    Initial commit

---
+++ root.js
@@ -0,0 +1,32 @@
+
+const express = require('express')
+const app = express()
+const path = require('path');
+const {rateLimit} = require('express-rate-limit')
+
+const limiter = rateLimit({
+	windowMs: 60 * 1000, // 15 minutes
+	limit: 10, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+    standardHeaders: true,
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+	message: {
+        status: false,
+        message: 'Rate limit exceeded'
+    }
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname+'/index.html'))
+})
+app.post('/status', (req, res) => {
+    res.json({
+      status: true,
+      message: "Server is OK"
+    })
+  })
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log('Start server at port 5000.')
+})
+
+app.use(limiter)

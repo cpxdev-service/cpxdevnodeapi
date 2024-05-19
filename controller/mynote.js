05d4e73ec3bc3d021d5bf2a86f70c6178aef05d2
Chinnathorn P <chinnathorn.p@gmail.com>
2024-05-19 18:49:27 +0700

    Update insert and update api

--- controller/mynote.js
+++ controller/mynote.js
@@ -6,6 +6,8 @@ const { createPool} = require('mysql2');
 const constr = ('mysql://avnadmin:AVNS_ysCQDNAWaVQYdvKTwma@cpxdevdb-cpxdevservice.aivencloud.com:21013/nodejsdemo');
 const basepath = '/mynote'
 
+const exjson = express.json();
+
 app.get(basepath + '/', (req, res) => {
   const connection = createPool(constr);
   connection.query('SELECT * from addNote', function (error, results, fields) {
@@ -58,7 +60,7 @@ app.get(basepath + '/:noteId', (req, res) => {
 
   });
 })
-app.post(basepath + '/', express.json(), (req, res) => {
+app.post(basepath + '/', exjson, (req, res) => {
     let body = req.body;
    if (JSON.stringify(body) == '{}') {
       res.status(403).json({
@@ -89,10 +91,17 @@ app.post(basepath + '/', express.json(), (req, res) => {
       return;
     }
 
-    res.json({
-        status: true,
-        noteId: nid
+    if (results.affectedRows > 0) {
+      res.status(201).json({
+          status: true,
+          noteId: nid
+        });
+    } else {
+      res.status(403).json({
+        status: false,
+        message: 'Data not changed'
       });
+    }
 
   });
    } catch (ex) {
@@ -102,10 +111,54 @@ app.post(basepath + '/', express.json(), (req, res) => {
       });
    }
 })
-app.put(basepath + '/:noteId', (req, res) => {
-    res.json({
-        status: true
-    })
+app.put(basepath + '/:noteId', exjson, (req, res) => {
+  let body = req.body;
+  if (JSON.stringify(body) == '{}') {
+     res.status(403).json({
+       status: false,
+       message: 'Data is empty.'
+     });
+     return;
+  }
+  const datacheck = JSON.stringify(body).toLowerCase();
+  if (!(datacheck.includes('noteheader') && datacheck.includes('notedesc'))) {
+     res.status(403).json({
+       status: false,
+       message: 'Data is wrong format.'
+     });
+     return;
+  }
+  body = JSON.parse(JSON.stringify(body).toLowerCase());
+  try {
+   const connection = createPool(constr);
+ connection.query('UPDATE `nodejsdemo`.`addNote` SET `noteHeader` = ?,'+
+ '`noteDesc` = ?,`noteUpdated` = UTC_TIMESTAMP() WHERE `noteId` = ?;', [ body.noteheader, body.notedesc, req.params.noteId ], function (error, results, fields) {
+   if (error) {
+     res.status(502).json({
+       status: false,
+       message: error.message
+     });
+     return;
+   }
+
+   if (results.affectedRows > 0) {
+    res.status(202).json({
+        status: true,
+        noteId: req.params.noteId
+      });
+  } else {
+    res.status(403).json({
+      status: false,
+      message: 'Data not changed'
+    });
+  }
+ });
+  } catch (ex) {
+     res.status(502).json({
+       status: false,
+       message: ex.message
+     });
+  }
 })
 app.delete(basepath + '/:noteId', (req, res) => {
     res.json({

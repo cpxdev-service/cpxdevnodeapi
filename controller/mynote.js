5b5816a7a2d953e1b758574e90c92a757272e5a3
Chinnathorn P <chinnathorn.p@gmail.com>
2024-05-19 18:22:20 +0700

    Update id

--- controller/mynote.js
+++ controller/mynote.js
@@ -1,10 +1,13 @@
 const { Router } = require('express'); 
+const { v4 } = require('uuid');
+const express = require('express')
 const app = Router(); 
 const { createPool} = require('mysql2');
-const connection = createPool('mysql://avnadmin:AVNS_ysCQDNAWaVQYdvKTwma@cpxdevdb-cpxdevservice.aivencloud.com:21013/nodejsdemo');
+const constr = ('mysql://avnadmin:AVNS_ysCQDNAWaVQYdvKTwma@cpxdevdb-cpxdevservice.aivencloud.com:21013/nodejsdemo');
 const basepath = '/mynote'
 
 app.get(basepath + '/', (req, res) => {
+  const connection = createPool(constr);
   connection.query('SELECT * from addNote', function (error, results, fields) {
     if (error) {
       res.status(502).json({
@@ -30,6 +33,7 @@ app.get(basepath + '/', (req, res) => {
   });
 });
 app.get(basepath + '/:noteId', (req, res) => {
+  const connection = createPool(constr);
   connection.query('SELECT * from addNote WHERE noteId=? LIMIT 1', [ req.params.noteId ], function (error, results, fields) {
     if (error) {
       res.status(502).json({
@@ -54,19 +58,49 @@ app.get(basepath + '/:noteId', (req, res) => {
 
   });
 })
-app.post(basepath + '/', (req, res) => {
-    const body = req.body;
-    try {
-      JSON.parse(body);
-    } catch(e) {
+app.post(basepath + '/', express.json(), (req, res) => {
+    let body = req.body;
+   if (JSON.stringify(body) == '{}') {
       res.status(403).json({
         status: false,
-        message: 'Request body should be in JSON format.'
-    })
+        message: 'Data is empty.'
+      });
+      return;
+   }
+   const datacheck = JSON.stringify(body).toLowerCase();
+   if (!(datacheck.includes('noteheader') && datacheck.includes('notedesc'))) {
+      res.status(403).json({
+        status: false,
+        message: 'Data is wrong format.'
+      });
+      return;
+   }
+   body = JSON.parse(JSON.stringify(body).toLowerCase());
+   const nid = v4();
+   try {
+    const connection = createPool(constr);
+  connection.query('INSERT INTO `nodejsdemo`.`addNote`(`noteId`,`noteHeader`,`noteDesc`,`noteCreated`,`noteUpdated`)VALUES'+
+'(?,?,?,UTC_TIMESTAMP(),UTC_TIMESTAMP());', [ nid, body.noteheader, body.notedesc ], function (error, results, fields) {
+    if (error) {
+      res.status(502).json({
+        status: false,
+        message: error.message
+      });
+      return;
     }
+
     res.json({
-        status: true
-    })
+        status: true,
+        noteId: nid
+      });
+
+  });
+   } catch (ex) {
+      res.status(502).json({
+        status: false,
+        message: ex.message
+      });
+   }
 })
 app.put(basepath + '/:noteId', (req, res) => {
     res.json({

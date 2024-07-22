cd66bb6b24993f374e4aec17bebeb472a0119822
Chinnathorn P <chinnathorn.p@gmail.com>
2024-07-22 14:06:32 +0700

    authen regis

--- controller/auth.js
+++ controller/auth.js
@@ -5,15 +5,31 @@ const app = Router();
 const jwt = require("jsonwebtoken");
 const os = require("os");
 var md5 = require('md5');
+const { MongoClient } = require("mongodb");
 
 const exjson = express.json();
 
-app.post("/token", (req, res) => {
+app.post("/token", exjson, async (req, res) => {
+  const client = new MongoClient(process.env.mongocon);
+  const database = client.db("nodejsdemo");
+  const movies = database.collection("authUser");
+  const response = await movies.findOne(
+    { user: req.body.userName.toLowerCase() }
+  );
+  client.close();
+  if (response == null) {
+    res.json({
+      status: false,
+      message: "User is already registered",
+    });
+    return;
+  }
+
   let jwtSecretKey = process.env.JWT_SECRET_KEY;
   let checksum = {
     time: Date(),
     host: os.hostname(),
-    userId: v4(),
+    userId: req.body.userName.toLowerCase(),
   };
   let data = {
     time: checksum.time,
@@ -31,11 +47,45 @@ app.post("/token", (req, res) => {
     access_token: token,
   });
 });
+app.post("/register", exjson, async (req, res) => {
+  const client = new MongoClient(process.env.mongocon);
+  try {
+    client.connect();
+    const database = client.db("nodejsdemo");
+    const movies = database.collection("authUser");
+    const response = await movies.findOne(
+      { userName: req.body.userName.toLowerCase() }
+    );
+    if (response != null) {
+      client.close();
+      res.json({
+        status: false,
+        message: "User is already registered",
+      });
+      return;
+    }
+    await movies.insertOne({
+      userName: req.body.userName.toLowerCase(),
+      userCreated: new Date()
+    });
+    client.close();
+    res.json({
+      status: true,
+      userName: req.body.userName.toLowerCase()
+    });
+  } catch (e) {
+    client.close();
+    res.json({
+      status: false,
+      message: e.message,
+    });
+  }
+});
+
 app.get("/getunix", (req, res) => {
   res.json({
     status: true,
     unix: Math.floor(+new Date() / 1000),
   });
 });
-
 module.exports = app;

fcc58331b031a1a6effdc5e635a0e23f35d5cb60
cpx2017 <56143786+cpx2017@users.noreply.github.com>
2024-11-12 11:55:01 +0700

    Update thaibankingslipvalid.js

--- controller/thaibankingslipvalid.js
+++ controller/thaibankingslipvalid.js
@@ -4,6 +4,7 @@ const { valid, getUser } = require("../service/token");
 const { v4 } = require("uuid");
 const { slipVerify } = require("promptparse/validate");
 const { anyId } = require("promptparse/generate");
+const axios = require('axios');
 
 app.get("/generate/:id/:amount", async (req, res) => {
   if (!valid(req, res)) {
@@ -73,6 +74,44 @@ app.get("/validation/:code", async (req, res) => {
     status: true,
     response: data,
   });
+});
+
+app.get("/validationapi/:code/:amount/:key", async (req, res) => {
+  if (!valid(req, res)) {
+    res.status(401).json({
+      status: false,
+      message: "Token is expired or not found",
+    });
+    return;
+  }
+
+  let data = JSON.stringify({
+  "refNbr": req.params.code,
+  "amount": req.params.amount,
+  "token": req.params.key
+});
+
+let config = {
+  method: 'post',
+  maxBodyLength: Infinity,
+  url: 'https://api.openslipverify.com',
+  headers: { 
+    'Content-Type': 'application/json'
+  },
+  data : data
+};
+
+axios.request(config)
+.then((response) => {
+  res.status(200).json({
+      status: true,
+      response: response.data,
+    });
+    return;
+})
+.catch((error) => {
+  console.log(error);
+});
 });
 
 module.exports = app;
